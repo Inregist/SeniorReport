582a0cd5daa76f02efe2c8a766447fb42310b0bb
inregist <inregist.irg@gmail.com>
2021-11-22 22:31:23 +0700

    build script

---
+++ build.sh
@@ -0,0 +1,8 @@
+xelatex report.tex -jobname=No23_1011-1064_Aj-Natasha_Report
+xelatex report.tex -jobname=No23_1011-1064_Aj-Natasha_Report
+
+bibtex No23_1011-1064_Aj-Natasha_Report
+bibtex No23_1011-1064_Aj-Natasha_Report
+
+xelatex report.tex -jobname=No23_1011-1064_Aj-Natasha_Report
+xelatex report.tex -jobname=No23_1011-1064_Aj-Natasha_Report
